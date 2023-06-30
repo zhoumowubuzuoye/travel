5e5e54a1defeb126d7b022f8d60bc578c4fc8922
Xie wenhao <1078637049@qq.com>
2023-06-30 14:00:11 +0800

    cesbase

--- react-travel/src/dd.ts
+++ react-travel/src/dd.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: xiewenhao
  * @Date: 2023-05-30 11:16:02
- * @LastEditTime: 2023-05-30 11:17:11
+ * @LastEditTime: 2023-06-30 13:59:53
  * @Description: 
  */
 export default {
-  name: "zsd",
+  name: "zsd1",
 };
